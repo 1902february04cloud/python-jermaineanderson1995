e4c0da9f4956c955b102d0276dd7f83614b7d351
Jermaine Anderson <jermaineanderson1995@localhost.localdomain>
2019-02-11 11:46:03 -0500

    Assignment Update

--- Jermaine-Anderson.py
+++ Jermaine-Anderson.py
@@ -1,40 +1,8 @@
 #!/usr/bin/env python3
 
 '''
-Revature is building a new API! This API contains functions for validating data, 
-solving problems, and encoding data. 
+General Instructions are in the README.md file
 
-The API consists of 10 functions that you must implement.
-
-Guidelines:
-1) Edit the file to match your first name and last name with the format shown.
-
-2) Provide tests in the main method for all functions, We should be able to run
-this script and see the outputs in an organized manner.
-
-3) You can leverage the operating system if needed, however, do not use any non
-legacy command that solves the problem by just calling the command.
-
-4) We believe in self commenting code, however, provide comments to your solutions
-and be organized.
-
-5) Leverage resources online if needed, but remember, be able to back your solutions
-up since you can be asked.
-
-6) Plagiarism is a serious issue, avoid it at all costs.
-
-7) Don't import external libraries which are not python native
-
-8) Don't change the parameters or returns, follow the directions.
-
-9) Assignment is optional, but totally recommend to achieve before Monday for practice.
-
-Happy Scripting!
-
-© 2018 Revature. All rights reserved.
-'''
-
-'''
 Use the main function for testing purposes and to show me results for all functions.
 '''
 
@@ -43,7 +11,6 @@ import math
 import logging
 import logging.config
 import os
-
 
 def main():
 	print('\n')
--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # python-assigment
 
-Instructions within script.
+Revature is building a new API! This API contains functions for validating data, 
+solving problems, and encoding data. 
+
+The API consists of 10 functions that you must implement.
+
+Guidelines:
+1) Edit the file to match your first name and last name with the format shown.
+
+2) Provide tests in the main method for all functions, We should be able to run
+this script and see the outputs in an organized manner.
+
+3) You can leverage the operating system if needed, however, do not use any non
+legacy command that solves the problem by just calling the command.
+
+4) We believe in self commenting code, however, provide comments to your solutions
+and be organized.
+
+5) Leverage resources online if needed, but remember, be able to back your solutions
+up since you can be asked.
+
+6) Plagiarism is a serious issue, avoid it at all costs.
+
+7) Don't import external libraries which are not python native
+
+8) Don't change the parameters or returns, follow the directions.
+
+9) Assignment is optional, but totally recommend to achieve before Monday for practice.
+
+Happy Scripting!
+
+© 2018 Revature. All rights reserved.
